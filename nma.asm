df7c76b5b814866a530a26b5872b803dc1ccb070
Artem Shkilniuk <a.shkilniuk@ukma.edu.ua>
2025-03-25 16:28:37 +0200

    Add reading arguments
    Added reading argument which identifies the name of he file to open

--- nma.asm
+++ nma.asm
@@ -1,18 +1,43 @@
 .model tiny
 .code
 org 100h
+.data
+filename db 255 dup(?), '$'
+
 start:
-    call print_message
-    
+    call get_filename
+    call print_message 
+
     mov ax, 4C00h
     int 21h
 
 print_message proc
-    mov dx, offset msg
+    mov dx, offset filename
     mov ah, 09h
     int 21h
     ret
 print_message endp
+
+
+get_filename proc
+    mov ax, @data    ; delete in .com
+    mov es, ax       ; delete in .com
+
+    xor ch, ch
+    mov cl, ds:[80h]
+    dec cl
+    jle read_end
     
-msg db '24', 0Dh, 0Ah, '$'
+    mov si, 82h
+    lea di, filename
+    cld
+    rep movsb
+
+    mov ds, ax       ; delete in .com
+
+    mov [di], byte ptr '$'
+read_end:
+    ret
+get_filename endp
+
 end start
