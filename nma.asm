b63afe8b6f823aa325345cd50a5d30642072914b
Artem Shkilniuk <a.shkilniuk@ukma.edu.ua>
2025-03-25 18:16:32 +0200

    Add writing of the string
    Rewrite the string to other variable for the further manipulations

--- nma.asm
+++ nma.asm
@@ -3,24 +3,30 @@
 org 100h
 .data
 filename db 255 dup(?), '$'
-file_handle dw ?
-file db 5 dup(?)
 buffer db 32000 dup(?)
 buffer_size dw 32000
-
+string db 32768 dup(?), '$'
+start_of_rules dw ?
+len_of_string dw ?
+file_handle dw ?
 start:
     call get_filename
     call open_file
     call read_file
     call close_file
-    call print_message 
+    call writing_string
+    call print_message
 
+exit:
     mov ax, 4C00h
     int 21h
 
 print_message proc
-    mov dx, offset filename
-    mov ah, 09h
+    mov ax, @data
+    mov ds, ax
+    mov es, ax
+    mov dx, offset string
+    mov ax, 0900h
     int 21h
     ret
 print_message endp
@@ -47,8 +53,7 @@ read_end:
 get_filename endp
 
 open_file proc
-    mov ah, 3dh
-    mov al, 00h
+    mov ax, 3d00h
     lea dx, filename
     int 21h
     mov file_handle, ax
@@ -56,8 +61,7 @@ open_file proc
 open_file endp
 
 read_file proc
-    mov ah, 3Fh
-    xor al, al
+    mov ax, 3F00h
     mov bx, file_handle
     lea dx, buffer
     mov cx, buffer_size
@@ -66,11 +70,29 @@ read_file proc
 read_file endp
 
 close_file proc
-    mov ah, 3Eh
-    xor al, al
+    mov ax, 3E00h
     mov bx, file_handle
     int 21h
     ret
 close_file endp
+
+writing_string proc
+    lea si, buffer
+    mov cx, word ptr [si]
+    add si, 4
+    add si, cx
+
+    mov ch, byte ptr [si + 1]
+    mov cl, byte ptr [si]
+    add si, 4
+    sub cx, 2
+    mov len_of_string, cx
+    lea di, string
+    cld
+    rep movsb
+    add si, 2
+    mov start_of_rules, si 
+    ret
+writing_string endp
 
 end start
