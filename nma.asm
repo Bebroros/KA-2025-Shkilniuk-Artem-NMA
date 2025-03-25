11df3b430a295036764f74d92c2035a155b7bc93
Artem Shkilniuk <a.shkilniuk@ukma.edu.ua>
2025-03-25 19:52:34 +0200

    Add reading rules sequentially
    Reading files using rule_count variable which identifies what rule to use and checks whether the state is final

--- nma.asm
+++ nma.asm
@@ -1,25 +1,86 @@
 .model tiny
 .code
-org 100h
+stack 100h
 .data
+string db 32768 dup(?), '$'
 filename db 255 dup(?), '$'
 buffer db 32000 dup(?)
 buffer_size dw 32000
-string db 32768 dup(?), '$'
 start_of_rules dw ?
 len_of_string dw ?
 file_handle dw ?
+rule_before dw ?
+len_before dw ?
+rule_after dw ?
+len_after dw ?
+rule_count dw 1d
+ending_flag db 0d
+.code
 start:
     call get_filename
     call open_file
     call read_file
     call close_file
     call writing_string
+    call reading_rules
     call print_message
-
 exit:
     mov ax, 4C00h
     int 21h
+
+reading_rules proc
+    mov bx, rule_count          ; counter of the rule to use
+    mov si, start_of_rules
+    mov dh, byte ptr [si + 1]   ; rule section counter
+    mov dl, byte ptr [si]
+    add si, 4
+reading_loop:
+    dec rule_count
+    mov rule_before, si
+    xor cx, cx
+rule_b:
+    lodsb
+    inc cx
+    dec dx
+    cmp al, 09h  
+    jne rule_b
+    dec cx
+    mov len_before, cx
+    mov rule_after, si
+    xor cx, cx
+rule_a:
+    lodsb
+    inc cx
+    dec dx    
+    cmp al, 09h
+    jne rule_a
+    dec cx
+    mov len_after, cx
+    mov rule_after, si
+skip_comment:
+    lodsb
+    dec dx
+    cmp al, 0Dh
+    jne skip_comment
+    lodsb
+    cmp al, 0Ah
+    jne skip_comment
+    cmp rule_count, 0
+    jne reading_loop
+final_state:
+    cmp len_after, 0
+    je return
+
+    add si, len_after
+    dec si
+
+    cmp [si], byte ptr '.'
+    jne return
+    dec len_after
+    inc ending_flag
+return:
+    ret
+reading_rules endp
 
 print_message proc
     mov ax, @data
