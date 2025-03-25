0223ae51a356a815af7493e2fd437d5e112d757c
Artem Shkilniuk <a.shkilniuk@ukma.edu.ua>
2025-03-25 16:35:08 +0200

    Add opening and reading of the file
    Opening of the file and write it to the buffer variable

--- nma.asm
+++ nma.asm
@@ -3,9 +3,16 @@
 org 100h
 .data
 filename db 255 dup(?), '$'
+file_handle dw ?
+file db 5 dup(?)
+buffer db 32000 dup(?)
+buffer_size dw 32000
 
 start:
     call get_filename
+    call open_file
+    call read_file
+    call close_file
     call print_message 
 
     mov ax, 4C00h
@@ -17,7 +24,6 @@ print_message proc
     int 21h
     ret
 print_message endp
-
 
 get_filename proc
     mov ax, @data    ; delete in .com
@@ -39,5 +45,32 @@ get_filename proc
 read_end:
     ret
 get_filename endp
+
+open_file proc
+    mov ah, 3dh
+    mov al, 00h
+    lea dx, filename
+    int 21h
+    mov file_handle, ax
+
+open_file endp
+
+read_file proc
+    mov ah, 3Fh
+    xor al, al
+    mov bx, file_handle
+    lea dx, buffer
+    mov cx, buffer_size
+    int 21h
+    ret
+read_file endp
+
+close_file proc
+    mov ah, 3Eh
+    xor al, al
+    mov bx, file_handle
+    int 21h
+    ret
+close_file endp
 
 end start
